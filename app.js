e9aebe0d7afaf384410fd05a5a198e42ed7d762d
pallavk09 <54513795+pallavk09@users.noreply.github.com>
2024-10-30 13:16:19 +0530

    Update app.js
    test get route added

--- app.js
+++ app.js
@@ -34,6 +34,7 @@ app.use(
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.get("/", (req, res) => res.send("Express on Vercel"));
 //Mouting Routers
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/student", studentRouter);
